23581b76989f7e2d6d4092c6458974344077ffdd
Edwin Puertas <eapuerta@gmail.com>
2026-02-09 15:15:52 -0500

    Integrate MDX and update Next.js configuration
    Add MDX support and configure Next.js options

--- next.config.js
+++ next.config.js
@@ -1,1 +1,19 @@
+import mdx from "@next/mdx";
 
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',            // requerido para export estático
+  images: { unoptimized: true }, // evitar el Image Optimizer (no corre en Pages)
+  trailingSlash: true,         // minimiza 404 en GitHub Pages
+  // Si publicas en la raíz (edwinpuertas.github.io), NO uses basePath/assetPrefix
+  // basePath: '',
+  // assetPrefix: '',
+};
+
+
+module.exports = nextConfig;
